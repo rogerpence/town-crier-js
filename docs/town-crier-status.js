adff8f908ec276ce3db54ae670a4a080436cc545
Roger Pence <roger.pence@gmail.com>
2019-07-24 13:38:19 -0500

    add docs folder

---
+++ docs/town-crier-status.js
@@ -0,0 +1,9 @@
+
+export const crierStatus = [
+    {"status": "primary", "color": "#cce5ff", "icon": "fas fa-check-circle" },
+    {"status": "secondary", "color": "#e2e3e5", "icon": "fa fa-bullhorn" },
+    {"status": "success", "color": "#d4edda", "icon": "fas fa-thumbs-up" },
+    {"status": "danger", "color": "#e83a30", "icon": "fas fa-exclamation-triangle" },
+    {"status": "warning", "color": "#F4FA58", "icon": "fas fa-exclamation-circle" },
+    {"status": "info", "color": "#d1ecf1", "icon": "fas fa-info-circle" }
+]
